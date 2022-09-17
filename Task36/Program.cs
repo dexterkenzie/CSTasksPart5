e5e2359458bf72a129cfb4acce43e0a867749db2
«dashkovsky» <>
2022-09-17 17:19:52 +0300

    task 36 done

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -4,3 +4,34 @@
 
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Задайте количество чисел в массиве:");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.Write("Массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int z = 0; z < numbers.Length; z += 2)
+    sum = sum + numbers[z];
+
+Console.WriteLine($"Сумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(-99, 100);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
